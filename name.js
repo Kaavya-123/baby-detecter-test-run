ff506f3bdc27a17eb483ddd56b2d8e9fa3d9ba3d
Kaavya-123 <81299350+Kaavya-123@users.noreply.github.com>
2021-11-25 15:56:08 +0530

    Update name.js

--- name.js
+++ name.js
@@ -2,7 +2,7 @@ objects = [];
 status = "";
 
 function preload() {
-  aud = loadSound("audio.wav");
+  sound = loadSound("audio.wav");
 }
 
 
@@ -13,11 +13,11 @@ function setup() {
   video.size(380, 380);
   video.hide();
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  document.getElementById("thing").innerHTML = "Status : Detecting Objects";
 }
 
 function modelLoaded() {
-  console.log("Model Loaded!")
+  console.log("Model Loaded!");
   status = true;
 }
 
@@ -29,7 +29,6 @@ function gotResult(error, results) {
   objects = results;
 }
 
-
 function draw() {
   image(video, 0, 0, 380, 380);
   if (status != "") {
@@ -37,34 +36,20 @@ function draw() {
     g = random(255);
     b = random(255);
     objectDetector.detect(video, gotResult);
-    for (i = 0; i < objects.length; i++) {
-      document.getElementById("status").innerHTML = "Status : Object Detected";
+    for (i = 0; i > objects.length; i++) {
+      document.getElementById("thing").innerHTML = "Status : Object Detected";
       document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
-      aud.play()
+      sound.stop();
       fill(r, g, b);
       percent = floor(objects[i].confidence * 100);
-      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+      text(objects[i].label + " ",percent + "%", objects[i].x + 15, objects[i].y + 15);
       noFill();
       stroke(r, g, b);
       rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
     }
   } else {
-    aud.stop()
-    document.getElementById("status").innerHTML = "Status: Baby is not detected";
+    sound.play();
+    document.getElementById("thing").innerHTML = "Status: Baby is not detected";
 
-  }
-}
-
-function draw() {
-  if (status != "") {
-    for (i = 0; i < objects.length; i++) {
-      percent = floor(objects[i].confidence * 100);
-      aud.play()
-      document.getElementById("status").innerHTML = "Status: Baby is detected";
-
-    }
-  } else {
-    aud.stop()
-    document.getElementById("status").innerHTML = "Status: Baby is not detected";
   }
 }
